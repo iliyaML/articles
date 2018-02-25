596bab00b74cc1b6d2b147cbfd59f0e3f272a91e
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-02-25 11:21:48 -0600

    Initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi obcaecati laborum dolore esse quibusdam nulla sed cumque blanditiis eveniet sapiente eum nostrum at, ut quidem similique facere cum distinctio voluptatem.',
+            'author': 'Iliya Mohamad Lokman',
+            'create_data': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi obcaecati laborum dolore esse quibusdam nulla sed cumque blanditiis eveniet sapiente eum nostrum at, ut quidem similique facere cum distinctio voluptatem.',
+            'author': 'Iliya Mohamad Lokman',
+            'create_data': '05-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi obcaecati laborum dolore esse quibusdam nulla sed cumque blanditiis eveniet sapiente eum nostrum at, ut quidem similique facere cum distinctio voluptatem.',
+            'author': 'Iliya Mohamad Lokman',
+            'create_data': '06-25-2017'
+        }
+    ]
+    return articles
